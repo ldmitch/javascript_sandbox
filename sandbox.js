b98ab578329ecf020759b9ac1e37fee36f3f9332
Liam Mitchell <111476886+ldmitch@users.noreply.github.com>
2023-06-08 09:58:10 -0400

    Practice with Strings

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,3 @@
-let age = 25;
-let currentYear = 2023; // use camel case
-// let 4yes = "string" (cannot start with a number, but can include one in other places)
-console.log(age, currentYear);
-
-const points = 100;
-console.group(points);
-
 /* VARIABLE TYPES:
 Number: integers, demicals (all floating point in JS)
 String: regular strings
@@ -15,3 +7,34 @@ Undefined: for variables that have not yet been defined
 Object: complex data structures (arrays, dates, literals, etc.)
 Symbol: used with objects
 */
+
+/* LOOSELY-TYPED LANGUAGE:
+Variables can be reassigned to different types in JavaScript
+*/
+
+// STRINGS:
+console.log("Hello, World");
+
+let email = "liam.mitchell@uwaterloo.ca";
+console.log(email);
+
+const firstName = "Liam";
+const lastName = "Mitchell";
+console.log(firstName + " " + lastName);
+
+const fullName = firstName + " " + lastName;
+console.log(fullName[0]); // "L"
+
+console.log(fullName.length); // String property
+console.log(fullName.toUpperCase()); // String method (needs brackets)
+
+let index = email.indexOf("@");
+console.log(email[index]); // "@"
+console.log(email.substring(index + 1)); // domain, "uwaterloo.ca"
+
+console.log(email.lastIndexOf(".")) // finds last occurance of "."
+console.log(email.slice(0, 10)); // sliced string from index 0 to 10 ("liam.mitch")
+console.log(email.substring(4, 10)); // substring from index 4 to 4+10 (second argument is length from start index)
+console.log(email.replace("liam.mitchell", "l5mitche")) // "l5mitche@uwaterloo.ca"
+
+// NUMBERS:
