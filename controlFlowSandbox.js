010065bedb516d028a55c0d5797f36185a217bd5
Liam Mitchell <111476886+ldmitch@users.noreply.github.com>
2023-06-08 16:34:24 -0400

    Practiced for loops

--- controlFlowSandbox.js
+++ controlFlowSandbox.js
@@ -1,5 +1,13 @@
 // FOR LOOPS:
+console.log("FOR LOOPS:");
 
 for(let i = 0; i < 5; i ++) {
+    console.log("in loop: ", i);
+}
 
+const names = ["liam", "curtis", "agishan"];
+for (let i = 0; i < names.length; i ++) {
+    console.log(names[i]);
+    let html = `<div>${names[i]}</div>`;
+    console.log(html);
 }
