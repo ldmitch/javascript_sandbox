691c02106ba141d4aa956d4098fbed6f1833f388
Liam Mitchell <111476886+ldmitch@users.noreply.github.com>
2023-06-08 16:38:39 -0400

    Practiced while loops

--- controlFlowSandbox.js
+++ controlFlowSandbox.js
@@ -2,7 +2,7 @@
 console.log("FOR LOOPS:");
 
 for(let i = 0; i < 5; i ++) {
-    console.log("in loop: ", i);
+    console.log("in loop:", i);
 }
 
 const names = ["liam", "curtis", "agishan"];
@@ -11,3 +11,24 @@ for (let i = 0; i < names.length; i ++) {
     let html = `<div>${names[i]}</div>`;
     console.log(html);
 }
+
+
+// WHILE LOOPS:
+console.log("WHILE LOOPS:");
+
+let i = 0;
+while(i < 5) {
+    console.log("in loop:", i);
+    i ++;
+}
+
+i = 0;
+while (i < names.length) {
+    console.log(names[i]);
+    i ++;
+}
+
+i = 5;
+do {
+    console.log(i);
+} while (i < 5)
