4aaa505b94b706c17a9e18dcfdc567b34941c647
Liam Mitchell <111476886+ldmitch@users.noreply.github.com>
2023-06-08 09:45:22 -0400

    Practicing JS variable usage

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,17 @@
-// alert("Hello World");
+let age = 25;
+let currentYear = 2023; // use camel case
+// let 4yes = "string" (cannot start with a number, but can include one in other places)
+console.log(age, currentYear);
+
+const points = 100;
+console.group(points);
+
+/* VARIABLE TYPES:
+Number: integers, demicals (all floating point in JS)
+String: regular strings
+Boolean: regular booleans
+Null: must be explicitly set to variables with no value
+Undefined: for variables that have not yet been defined
+Object: complex data structures (arrays, dates, literals, etc.)
+Symbol: used with objects
+*/
