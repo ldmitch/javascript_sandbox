ef4c0744030519744c955793b1f3620a2a49a8de
Liam Mitchell <111476886+ldmitch@users.noreply.github.com>
2023-06-08 20:06:04 -0400

    Initial commit for objects

---
+++ objects.js
@@ -0,0 +1,11 @@
+// OBJECT LITERALS:
+console.log("OBJECT LITERALS:");
+
+let user = {
+    name: "Liam",
+    age: 19,
+    email: "liam.mitchell@uwaterloo.ca",
+    location: "Waterloo"
+};
+console.log(user);
+console.log(user.email);
