305cb12fce29c4ee8272d2eee47205a68893e1de
Liam Mitchell <111476886+ldmitch@users.noreply.github.com>
2023-06-13 20:56:09 -0400

    Added a callback function

--- asynchronous.js
+++ asynchronous.js
@@ -1,20 +1,31 @@
 // ASYNCHRONOUS JAVASCRIPT:
 console.log("ASYNCHRONOUS JAVASCRIPT:");
 
-console.log(1);
-setTimeout(() => {
-    console.log("(2) Callback fired...")
-}, 2000);
-console.log(3)
+const getTodos = (callback) => {
+    console.log(1);
+    setTimeout(() => {
+        console.log("(2) Callback fired...")
+    }, 2000);
+    console.log(3)
 
-const request = new XMLHttpRequest();
-request.addEventListener("readystatechange", () => {
-    // console.log(request, request.readyState);
-    if (request.readyState === 4 && request.status === 200) {
-        console.log(request.responseText);
-    } else if (request.readyState === 4) {
-        console.log("Status code:", request.status)
+    const request = new XMLHttpRequest();
+    request.addEventListener("readystatechange", () => {
+        // console.log(request, request.readyState);
+        if (request.readyState === 4 && request.status === 200) {
+            callback(undefined, request.responseText);
+        } else if (request.readyState === 4) {
+            callback(request.status, undefined)
+        }
+    });
+    request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+    request.send();
+}
+
+getTodos((err, data) => {
+    console.log("Callback fired...");
+    if (err) {
+        console.log(`Code ${err}, could not fetch data...`);
+    } else {
+        console.log(data);
     }
 });
-request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
-request.send();
