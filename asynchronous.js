de63b2fadad392f8bf03d0c139e6ac4a96970538
Liam Mitchell <111476886+ldmitch@users.noreply.github.com>
2023-06-13 21:24:48 -0400

    Experimented with "callback hell"

--- asynchronous.js
+++ asynchronous.js
@@ -1,31 +1,41 @@
 // ASYNCHRONOUS JAVASCRIPT:
 console.log("ASYNCHRONOUS JAVASCRIPT:");
 
-const getTodos = (callback) => {
-    console.log(1);
-    setTimeout(() => {
-        console.log("(2) Callback fired...")
-    }, 2000);
-    console.log(3)
+console.log(1);
+setTimeout(() => {
+    console.log("First callback fired...")
+}, 2000);
+console.log(3)
+
+const getTodos = (resource, callback) => {
 
     const request = new XMLHttpRequest();
     request.addEventListener("readystatechange", () => {
         // console.log(request, request.readyState);
         if (request.readyState === 4 && request.status === 200) {
-            callback(undefined, request.responseText);
+            const data = JSON.parse(request.responseText);
+            callback(undefined, data);
         } else if (request.readyState === 4) {
             callback(request.status, undefined)
         }
     });
-    request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+    request.open("GET", resource);
     request.send();
 }
 
-getTodos((err, data) => {
+getTodos("todos.json", (err, data) => {
     console.log("Callback fired...");
     if (err) {
         console.log(`Code ${err}, could not fetch data...`);
     } else {
         console.log(data);
     }
+    getTodos("other.json", (err, data) => {
+        console.log("Callback fired...");
+        if (err) {
+            console.log(`Code ${err}, could not fetch data...`);
+        } else {
+            console.log(data);
+        }
+    });
 });
