68bd28e10ce8c84fb3ccc31f53714fa55b18885d
Liam Mitchell <111476886+ldmitch@users.noreply.github.com>
2023-06-14 07:51:44 -0400

    Practiced using Async and Await

--- fetch.js
+++ fetch.js
@@ -9,3 +9,19 @@ fetch("todos.json").then((response) => {
 }).catch((error) => {
     console.log("Rejected:", error);
 });
+
+
+// ASYNC & AWAIT:
+console.log("ASYNC & AWAIT:");
+
+const getOther = async () => { // returns a promise (non-blocking)
+    const response = await fetch("other.json"); // waits for the fetch() to finish before assigning to response
+    if (response.status !== 200) {
+        throw new Error("Cannot fetch data");
+    }
+    const data = await response.json()
+    return data;
+};
+getOther()
+    .then(data => console.log("Data:", data))
+    .catch(error => console.log("Error:", error.message));
